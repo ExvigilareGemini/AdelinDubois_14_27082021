b76556a468460a2bb49e5490a3d519d018d8868b
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-10-05 22:56:39 +0200

    Refactor : replacing configureStore with createStore

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -4,11 +4,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import reducer from "./redux/reducers/index"
 import { createStore } from "redux";
-import {configureStore} from "@reduxjs/toolkit"
 
-const store = configureStore({reducer: reducer})
-
-// const store = createStore(reducer)
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
